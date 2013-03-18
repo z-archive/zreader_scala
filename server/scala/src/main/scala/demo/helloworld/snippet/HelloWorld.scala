6c3fa44619e2a5829c02df0c96eefd6c12261b33
Oleg Tsarev <zabivator@gmail.com>
2013-03-19 00:14:08 +0900

    return HelloWorld from lift to original version

--- server/scala/src/main/scala/demo/helloworld/snippet/HelloWorld.scala
+++ server/scala/src/main/scala/demo/helloworld/snippet/HelloWorld.scala
@@ -1,8 +1,6 @@
 package demo.helloworld.snippet
 
-import xml.Elem
-
 class HelloWorld {
-  def main: Elem = <span>Welcome to helloworld at {new _root_.java.util.Date}</span>
+  def main = <span>Welcome to helloworld at {new _root_.java.util.Date}</span>
 }
 
