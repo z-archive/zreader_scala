47f672a4ebe154ad8fb41880dc0201284aa5f345
Oleg Tsarev <zabivator@gmail.com>
2013-03-21 04:23:48 +0900

    update Copyright headers

--- src/test/scala/sh/oleg/zreader/rss/RSSReaderTest.scala
+++ src/test/scala/sh/oleg/zreader/rss/RSSReaderTest.scala
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Z-Reader (c) 2013
+ *
+ * Z-Reader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Z-Reader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Z-Reader.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package sh.oleg.zreader.rss
 
 import org.specs2.mutable._
@@ -6,10 +23,6 @@ import org.specs2.runner.JUnitRunner
 import org.specs2.specification.Scope
 import sh.oleg.zreader.rss.EntityModule.Feed
 
-/**
- * User: Eugene Dzhurinsky
- * Date: 3/18/13
- */
 @RunWith(classOf[JUnitRunner])
 class RSSReaderTest extends Specification {
 
