498bcbcb275b7b785a79f0093a15fffca9721021
Oleg Tsarev <zabivator@gmail.com>
2013-03-21 04:14:34 +0900

    Added Site-Map and pages: 'login', 'index', 'read' and 'settings'

--- server/scala/src/main/scala/bootstrap/liftweb/Boot.scala
+++ server/scala/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,7 +1,25 @@
+/*
+ * This file is part of Z-Reader (c) 2013
+ *
+ * Z-Reader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Z-Reader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Z-Reader.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package bootstrap.liftweb
 
 import _root_.net.liftweb.http._
 import _root_.net.liftweb.sitemap._
+import Loc._
 
 /**
   * A class that's instantiated early and run.  It allows the application
@@ -12,13 +30,15 @@ class Boot {
     // where to search snippet
     LiftRules.addToPackages("sh.oleg.zreader")
 
-    // use HTML5 by default
-    LiftRules.htmlProperties.default.set((r: Req) =>
-      new Html5Properties(r.userAgent))
-
     // Build SiteMap
-    val entries = Menu(Loc("Home", List("index"), "Home")) :: Nil
-    LiftRules.setSiteMap(SiteMap(entries:_*))
+    val prefix = "Z-Reader"
+    def siteMap() : SiteMap = SiteMap(
+      Menu.i("index") / "index",
+      Menu.i("login" ) / "login",
+      Menu.i("read") / "read",
+      Menu.i("settings") / "settings"
+    )
+    LiftRules.setSiteMapFunc(() => siteMap())
   }
 }
 
--- server/scala/src/main/scala/sh/oleg/zreader/rss/EntityModule.scala
+++ server/scala/src/main/scala/sh/oleg/zreader/rss/EntityModule.scala
@@ -1,11 +1,22 @@
+/*
+ * This file is part of Z-Reader (c) 2013
+ *
+ * Z-Reader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Z-Reader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Z-Reader.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package sh.oleg.zreader.rss
 
-/**
- * User: Eugene Dzhurinsky
- * Date: 3/18/13
- *
- * Holds information about entities being used in the project.
- */
 object EntityModule {
 
   case class FeedItem(content: String, permLink: String, published: Long)
